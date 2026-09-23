99982f2cffb54cf9548b13a02131aba066a6b6a7
Safreen-1704 <safreenismail@gmail.com>
2026-09-23 21:32:03 +0530

    Update GFG solution: Odd or Even

---
+++ arrays/OddOrEven.java
@@ -0,0 +1,15 @@
+package arrays;
+
+class Solution {
+    static boolean isEven(int n) {
+        // code here
+       
+        if(n%2 == 0){
+            return(true);
+        }
+        else{
+            return(false);
+        }
+        
+    }
+}
